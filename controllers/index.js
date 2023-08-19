afc6720785f22cfb82d707358bf1845ae156bf77
Dahlia Guido <dguido.dev@gmail.com>
2023-08-18 22:57:39 -0500

    router

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const apiRoutes = require('./api');
 const router = require('express').Router();
+const homeRoutes = require('./homeRoute');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
