55b2955d81bd9804e08a627c143e1d376081a397
Dahlia Guido <dguido.dev@gmail.com>
2023-08-19 00:08:04 -0500

    express sessions module

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,24 +1,45 @@
-const router = require ('express').Router();
+const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 // Get all posts
-router.get('/', async (req, res) => {
-    try{
-        const result = await Post.findAll();
-        const posts = result.map((post) => {
-            return post.get({plain: true})
-        })
-        // res.status(200).json(posts);
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'content',
+        'user_id',
+        'created_at'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['name']
+          }
+        },
+        {
+          model: User,
+          attributes: ['name']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true}));
+
         res.render('home', {
             posts,
-            loggedIn: req.session.loggedIn
-        });
-    }
-    catch(err) {
+            loggedIn: req.session.loggedIn,
+            username: req.session.username
+        })
+    })
+      .catch(err => {
+        console.log(err);
         res.status(500).json(err);
-    }
-});
+      });
+  });
 
 // login
 router.get('/login', (req, res) => {
@@ -28,6 +49,11 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+});
+
+// signup
+router.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
 module.exports = router;
