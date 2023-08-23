fd39c53550cae1e2dc3e31fb33a42e41d26f466b
Dahlia Guido <dguido.dev@gmail.com>
2023-08-23 18:11:32 -0500

    login is still broken

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,13 @@
 console.log("linked");
-async function loginHandler(event) {
+const loginFormHandler = async (event) => {
     event.preventDefault();
 
+    // Collect values from the login form
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
     if (email && password) {
+        // Send a POST request to the API endpoint
         const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
@@ -18,11 +20,13 @@ async function loginHandler(event) {
         });
         console.log(response);
         if (response.ok) {
+            // If successful, redirect the browser to the profile page
             document.location.replace('/dashboard');
             console.log("logged in");
         } else {
             alert(response.statusText);
         }
     }
-}
-document.querySelector('.login-form').addEventListener('submit', loginHandler);
+};
+document.querySelector('.login-form')
+.addEventListener('submit', loginFormHandler);
