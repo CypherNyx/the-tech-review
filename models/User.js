063f1c194a0b09725f224fd7ae8201c6c1856343
Dahlia Guido <dguido.dev@gmail.com>
2023-08-23 21:57:48 -0500

    adding bcrypt hooks

--- models/User.js
+++ models/User.js
@@ -21,14 +21,14 @@ User.init(
       allowNull: false,
       unique: true,
     },
-    email:{
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
         isEmail: true,
-    }
-  },
+      }
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -46,6 +46,17 @@ User.init(
     // },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+
     sequelize,
     timestamps: true,
     freezeTableName: true,
